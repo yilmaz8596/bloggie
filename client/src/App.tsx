ebd6625e9e000f304a612294927b5aaaac1e0637
yilmaz8596 <yilmaz8596@gmail.com>
2024-11-13 11:24:40 +0300

    create the header section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,12 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
